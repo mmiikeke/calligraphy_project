585f32f5745180fca0bca0b963e26d58b716b531
mmiikeke <o860116@gmail.com>
2020-11-30 23:39:55 +0800

    Create class printer

---
+++ web crawler/show_class.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+
+if __name__ == '__main__':
+    filepath = os.path.realpath('data/data_去除無用資訊.csv')
+    df = pd.read_csv(filepath, header=None, low_memory=False)
+    series = df.loc[:,10]
+    #b = series.sort_values(ascending=True)
+    obj = list()
+    num = list()
+    for i in range(len(series)):
+        if not series[i] in obj:
+            obj.append(series[i])
+            num.append(1)
+        else:
+            location = obj.index(series[i])
+            num[location] += 1
+    
+    for i in range(len(obj)):
+        print(f'1\t{obj[i]}\t{num[i]}')
+
+    print(f'len = {len(obj)}')
