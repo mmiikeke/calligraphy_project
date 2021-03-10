8ab701771905a4b06283873e04077fcba4ec86d6
mmiikeke <o860116@gmail.com>
2021-03-10 11:57:35 +0800

    DenseNet-5 implementation

--- code/part1/cycleGAN/models.py
+++ code/part1/cycleGAN/models.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+# Residual block
 class ResidualBlock(nn.Module):
     def __init__(self, in_features):
         super(ResidualBlock, self).__init__()
@@ -18,11 +19,41 @@ class ResidualBlock(nn.Module):
     def forward(self, x):
         return x + self.conv_block(x)
 
+# DenseNet-5
+class DenseBlock(nn.Module):
+    def __init__(self, in_features):
+        super(DenseBlock, self).__init__()
+
+        self.relu = nn.ReLU(inplace = True)
+        self.bn = nn.BatchNorm2d(num_channels = in_features)
+        
+        self.conv1 = nn.Conv2d(in_channels = in_features, out_channels = 32, kernel_size = 3, stride = 1, padding = 1)
+        self.conv2 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 3, stride = 1, padding = 1)
+        self.conv3 = nn.Conv2d(in_channels = 64, out_channels = 32, kernel_size = 3, stride = 1, padding = 1)
+        self.conv4 = nn.Conv2d(in_channels = 96, out_channels = 32, kernel_size = 3, stride = 1, padding = 1)
+        self.conv5 = nn.Conv2d(in_channels = 128, out_channels = 32, kernel_size = 3, stride = 1, padding = 1)
+    
+    def forward(self, x):
+        bn = self.bn(x) 
+        conv1 = self.relu(self.conv1(bn))
+        conv2 = self.relu(self.conv2(conv1))
+
+        # Concatenate in channel dimension
+        c2_dense = self.relu(torch.cat([conv1, conv2], 1))
+        conv3 = self.relu(self.conv3(c2_dense))
+        c3_dense = self.relu(torch.cat([conv1, conv2, conv3], 1))
+        conv4 = self.relu(self.conv4(c3_dense))
+        c4_dense = self.relu(torch.cat([conv1, conv2, conv3, conv4], 1))
+        conv5 = self.relu(self.conv5(c4_dense))
+        c5_dense = self.relu(torch.cat([conv1, conv2, conv3, conv4, conv5], 1))
+    
+        return c5_dense
+
 class Generator(nn.Module):
-    def __init__(self, input_nc, output_nc, n_residual_blocks=6):
+    def __init__(self, input_nc, output_nc, use_densenet=False, n_residual_blocks=6):
         super(Generator, self).__init__()
 
-        # Initial convolution block       
+        # Initial convolution block
         model = [   nn.ReflectionPad2d(3),
                     nn.Conv2d(input_nc, 64, 7),
                     nn.InstanceNorm2d(64),
@@ -38,9 +69,12 @@ class Generator(nn.Module):
             in_features = out_features
             out_features = in_features*2
 
-        # Residual blocks
-        for _ in range(n_residual_blocks):
-            model += [ResidualBlock(in_features)]
+        # Transfer module
+        if not use_densenet:
+            for _ in range(n_residual_blocks):
+                model += [ResidualBlock(in_features)]
+        else:
+            model += [DenseBlock(in_features)]
 
         # Upsampling
         out_features = in_features//2
