339c9bfa43ccd7bdcd783e007356604ae6d5918c
mmiikeke <o860116@gmail.com>
2021-04-25 00:44:21 +0800

    batch size

--- code/part1/cycleGAN/main_train.py
+++ code/part1/cycleGAN/main_train.py
@@ -2,10 +2,12 @@ import os
 
 outpath = 'output_dense_48086817_pad'
 use_dense = True
+batchSize = 16
 dataroot = 'data/4808_6817_pad'
-os.system(f'python train.py --dataroot {dataroot} --cuda --outpath {outpath}{" --dense" if use_dense else ""}')
+os.system(f'python train.py --dataroot {dataroot} --batchSize {batchSize} --cuda --outpath {outpath}{" --dense" if use_dense else ""}')
 
 outpath = 'output_resnet_48086817_pad'
 use_dense = False
+batchSize = 16
 dataroot = 'data/4808_6817_pad'
-os.system(f'python train.py --dataroot {dataroot} --cuda --outpath {outpath}{" --dense" if use_dense else ""}')
+os.system(f'python train.py --dataroot {dataroot} --batchSize {batchSize} --cuda --outpath {outpath}{" --dense" if use_dense else ""}')
